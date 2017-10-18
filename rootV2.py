afb7951327dc5252aaae2fcf1c2564be6dfcab25
adithm <chess.play1234@gmail.com>
2017-10-18 16:43:55 +0530

    updated

--- rootV2.py
+++ rootV2.py
@@ -1,10 +1,6 @@
+import cmath
+
 def root(a, b, c):
-  sq = b * b - 4 * a * c
-  if sq < 0:
-    r1 = pow(abs(b * b - 4 * a * c), 0.5)
-    r2 = -b / 2 * a
-    return complex(r2, r1), complex(r2, -r1)
-  else:
-	  return (-b + pow(b * b - 4 * a * c, 0.5)) / (2 * a), (-b - pow(b * b - 4 * a * c, 0.5)) / (2 * a)
+	  return (-b + cmath.sqrt(b * b - 4 * a * c)) / (2 * a), (-b - cmath.sqrt(b * b - 4 * a * c)) / (2 * a)
 
 print root(2, 3, 5)
