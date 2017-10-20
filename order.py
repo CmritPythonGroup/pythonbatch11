27efe36bb40e789ea6edca29ac373dfb3ef6ba8c
adithm <chess.play1234@gmail.com>
2017-10-20 11:43:59 +0530

    changed

--- order.py
+++ order.py
@@ -2,53 +2,62 @@ class Menu:
   def __init__(self):
     self.d = {'vada' : 10, 'idly' : 20}
     
+  def __contains__(self, key):
+    return key in self.d
+    
   def __getitem__(self, i):
     try:
       return self.d[i]
-    except:
-      print('not in menu')
+    except Exception as error:
+      print 'not in menu. error - ' + repr(error)
         
   def __setitem__(self, key, item): 
     self.d[key] = item
     
   def show(self):
-    print 'menu'
+    print 'menu :'
     for i in self.d.items():
-      print str(i[0]) + ' costs ' + str(i[1])
+      print '\t' + str(i[0]) + ' costs ' + str(i[1])
       
   def add(self, a, b):
     self.d[a] = b
     
 class Order:
+  d = Menu()
   def __init__(self, m):
-    self.d = {}
+    self.d = m
     self.ord = {}
-    for i in m.d:
-      self.d[i] = m.d[i]
       
   def __getitem__(self, i):
-    return self.d[i]
+    try:
+      return self.ord[i]
+    except Exception as error:
+      print 'not in menu. error - ' + repr(error)
       
   def __setitem__(self, key, item): 
     try:
-      if self.d[key] == item:
+      #if self.d[key] == item:
+      if key in self.d:
         self.ord[key] = item
       else:
         print 'wrong price'
-    except:
-      print 'not in menu'
+    except Exception as error:
+      print 'not in menu. error - ' + repr(error)
       
   def show(self):
-    print 'order'
+    print 'order :'
     for i in self.ord.items():
-      print str(i[0]) + ' costs ' + str(i[1])
+      print '\t' + str(i[0]) + ' costs ' + str(i[1])
     
   
 c = Menu()
 c['idly'] = 20
+print c['i']
 c.show()
-o = Order(c)
+o1 = Order(c)
 o2 = Order(c)
-o['vada'] = 10
-o.show()
+o1['vada'] = 10
+o2['idly'] = 20
+print o1['vada']
+o1.show()
 o2.show()
