d09b783271b7084a0d43990e3c011b5b40811420
adithm <chess.play1234@gmail.com>
2017-11-12 20:56:41 +0530

    Update files.py

--- files.py
+++ files.py
@@ -1,4 +1,7 @@
-with open('first.txt', 'r') as inp:
-	with open('second.txt', 'w') as out:
-		for line in inp:
-			out.write(line)
+try:
+	with open('first.txt', 'r') as inp:
+		with open('second.txt', 'w') as out:
+			for line in inp:
+				out.write(line)
+except IOError:
+        print "Wrong file or file path"
